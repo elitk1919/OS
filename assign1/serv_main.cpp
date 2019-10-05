0c3e33e849f750323a3f316db429b81e041b5090
Eli Kolb <kolbet0356@uwec.edu>
2019-10-05 12:09:59 -0500

    updating server driver

--- assign1/serv_main.cpp
+++ assign1/serv_main.cpp
@@ -10,9 +10,15 @@ using namespace util;
 
 
 int main(int argc, char* argv[]) {
-	BLOWFISH bf(str2Hex(argv[1]));
+	BLOWFISH bf(str2Hex(COMMON_KEY));
 	ServerSocket ss(6969);
-	string message = bf.Encrypt_CBC("hello there");
+	cout << "Enter string S:" << endl;
+	string s;
+	s << cin;
+	cin.ignore();
+	string hex = str2Hex(s);
+	string message = bf.Encrypt_CBC(hex);
+	cout << "S encrypted:" << endl;	
 	Socket s = ss.acceptCli();
 	s << message;
 	

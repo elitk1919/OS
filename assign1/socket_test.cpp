947a1a54246e24344bd7e40bae7febfa34016460
KOLBET0356 <kolbet0356@Thing2.cs.uwec.edu>
2019-10-15 12:17:00 -0500

    Adding socket test file

--- assign1/socket_test.cpp
+++ assign1/socket_test.cpp
@@ -1,18 +1,75 @@
 #include "ServerSocket.h"
 #include <iostream>
+#include <fstream>
 #include <string>
 
+#define WRITE_SIZE 256
+
 using namespace std;
-int main(int arcg, char* argv[]) {
-    string arg1 = argv[1];
-    if (arg1 == "client") { 
-        Socket so(argv[2], 9420);
-        std::cout << so.readstring() << std::endl;
+
+bool sendFile(Socket ss, string fname) {
+    ifstream inFile;
+    inFile.open(fname);
+    string temp("");
+    string total("");
+    while (getline(inFile, temp)) {
+        total += temp + '\n';
     }
-    if (arg1 == "server") {
-        std::cout << "here";
-        ServerSocket ss(9420);
-        Socket so = ss.acceptCli();
-        so << "WAZZZZZZUPPPP";   
+    total.pop_back();
+    ss << "FILE_TRANSFER_INIT";
+    if (ss.readstring() != "FILE_TRANSFER_CONFIRM") return false;
+    string write = "";
+    for (int i = 0; i < total.size(); i++) { 
+        write += total[i];
+        if (i == WRITE_SIZE) { 
+            ss << write;
+            write = "";
+        }
+    }
+    ss << write;
+    ss << "FILE_TRANSFER_COMPLETE";
+    if (ss.readstring() == "TRANSFER_END_CONFIRM") return true;
+}
+
+bool readFile(Socket s, string outfile) {
+    if (s.readstring() != "FILE_TRANSFER_INIT") return false;
+    else s << "FILE_TRANSFER_CONFIRM";
+    string str = "";
+    string f = "";
+    while(str != "FILE_TRANSFER_COMPLETE") {
+        str = s.readstring();
+        cout << str;
+        if (str != "FILE_TRANSFER_COMPLETE") f += str;
+    }
+    s << "TRANSFER_END_CONFIRM";
+    //ofstream out;
+    //out.open(outfile, ios::binary);
+    cout << f << endl;
+    //out.close();
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        string strr = "hello";
+        if (strr != "Hello") {
+            cout << "Incorrent syntax. specify client or server" << endl;
+        }
+        //sendFile("sample.txt");
+    } else {
+        string arg1 = argv[1];
+        if (arg1 == "client") { 
+            Socket so(argv[2], 9420);
+            cout << "Client socket opened" << endl;
+            readFile(so, "newFile.txt");
+        }
+        if (arg1 == "server") {
+            cout << "ServerSocket opened" << endl;
+            ServerSocket ss(9420);
+            Socket so = ss.acceptCli();
+            cout << "sending file..." << endl;
+            sendFile(so, "decind.txt");
+            cout << "file sent" << endl;
+        }
     }
 }

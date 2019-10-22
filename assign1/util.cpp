faeaf5598f1edff7a622e740adaddb14f0a6183d
Paul <42386469+PaulkGithub@users.noreply.github.com>
2019-10-22 13:49:15 -0500

    Update util.cpp

--- assign1/util.cpp
+++ assign1/util.cpp
@@ -16,3 +16,30 @@ string util::str2Hex(string input) {
     }
     return output;
 }
+
+long util::f(long nonce) {
+    const long A = 48271;
+    const long M = 2147483647;
+    const long Q = M/A;
+    const long R = M%A;
+
+        static long state = 1;
+        long t = A * (state % Q) - R * (state / Q);
+
+        if (t > 0)
+                state = t;
+        else
+                state = t + M;
+        return (long)(((double) state/M)* nonce);
+}
+
+string util::random_string(int length) {
+    char alphanum[] ="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    int alphanumLength = sizeof(alphanum) - 1;
+    int i;
+    string random;
+    for (i = 0; i < length; i++) {
+        random += alphanum[rand() % alphanumLength];
+    }
+    return random;
+}
